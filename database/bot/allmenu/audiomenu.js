a2d176485cb43e8605dc9292cfd922b85c77010e
Naylachan <82255308+naylachan@users.noreply.github.com>
2022-03-23 06:30:51 +0800

    Delete database/bot directory

--- database/bot/allmenu/audiomenu.js
+++
@@ -1,7 +0,0 @@
-const audiomenu = (prefix, tz) => {
-	return `☞ *AUDIO* [ 20 ]
-Silahkan pilih audio 1-20
-Contohnya *${prefix}audio8*`
-}
-
-exports.audiomenu = audiomenu
--- database/bot/allmenu/islamimenu.js
+++
@@ -1,9 +0,0 @@
-const islamimenu = (prefix, tz) => {
-	return `☞ *ISLAMIC* [ 4 ]
-${tz} *${prefix}hadist*
-${tz} *${prefix}quran*
-${tz} *${prefix}asmaulhusna*
-${tz} *${prefix}kisahnabi*`
-}
-
-exports.islamimenu = islamimenu
